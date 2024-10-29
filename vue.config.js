5b7ae8693c104c4dbd484b7005d221180c77ddbe
pengxing <979698753@qq.com>
2024-10-29 21:52:52 +0800

    修改vue.config.js配置，使能展示svg图标

--- vue.config.js
+++ vue.config.js
@@ -29,21 +29,60 @@ module.exports = defineConfig({
         url: false
       }
     },
-    module: {
-      rules: [
-        {
-          test: /\.svg$/,
-          loader: 'svg-sprite-loader',
-          include: [
-            path.resolve(__dirname, 'src/icons')
-          ],
-          options: {
-            symbolId: 'icon-[name]'
-          }
-        }
-      ]
-    }
   },
+    chainWebpack(config) {
+      // it can improve the speed of the first screen, it is recommended to turn on preload
+      // it can improve the speed of the first screen, it is recommended to turn on preload
+      // config.plugin('preload').tap(() => [
+      //   {
+      //     rel: 'preload',
+      //     // to ignore runtime.js
+      //     // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
+      //     fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
+      //     include: 'initial'
+      //   }
+      // ])
+
+      // // when there are many pages, it will cause too many meaningless requests
+      // config.plugins.delete('prefetch')
+
+      // set svg-sprite-loader
+      config.module
+        .rule('svg')
+        .exclude.add(path.resolve(__dirname, 'src/icons/svg'))
+        .end()
+      config.module
+        .rule('worker')
+        .test(/\.worker\.mjs$/)
+        .use('worker-loader')
+        .loader('worker-loader')
+        .end()
+      config.module
+        .rule('icons')
+        .test(/\.svg$/)
+        .include.add(path.resolve(__dirname, 'src/icons/svg'))
+        .end()
+        .use('svg-sprite-loader')
+        .loader('svg-sprite-loader')
+        .options({
+          symbolId: 'icon-[name]'
+        })
+        .end()
+    },
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.svg$/,
+    //       loader: 'svg-sprite-loader',
+    //       include: [
+    //         path.resolve(__dirname, 'src/icons/svg')
+    //       ],
+    //       options: {
+    //         symbolId: 'icon-[name]'
+    //       }
+    //     }
+    //   ]
+    // }
   css: {
     loaderOptions: {
       // 给 sass-loader 传递选项
